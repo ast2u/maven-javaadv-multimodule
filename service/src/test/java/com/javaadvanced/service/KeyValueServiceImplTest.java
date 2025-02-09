e93fdaf92ab50a68d53370a3f9282e570871fa81
Carlo Astudillo <carloasto11@gmail.com>
2025-02-09 14:50:46 +0800

    Refactor code and remove unused imports

--- service/src/test/java/com/javaadvanced/service/KeyValueServiceImplTest.java
+++ service/src/test/java/com/javaadvanced/service/KeyValueServiceImplTest.java
@@ -1,27 +1,36 @@
 package com.javaadvanced.service;
 
-import com.javaadvanced.model.Row;
-import com.javaadvanced.model.KeyValuePair;
-import com.javaadvanced.model.SearchResult;
-import com.javaadvanced.utils.TxtFileHandler;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.stream.IntStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+import com.javaadvanced.model.KeyValuePair;
+import com.javaadvanced.model.Row;
+import com.javaadvanced.model.SearchResult;
+import com.javaadvanced.utils.TxtFileHandler;
 
 public class KeyValueServiceImplTest {
 
     @Mock
     private TxtFileHandler fileHandler;
-    
+
     private KeyValueServiceImpl kvServiceImpl;
 
     @BeforeEach
@@ -29,7 +38,7 @@ public class KeyValueServiceImplTest {
         MockitoAnnotations.openMocks(this);
         kvServiceImpl = new KeyValueServiceImpl("test.txt");
         kvServiceImpl.fileHandler = fileHandler;
-            
+
     }
 
     @Test
@@ -60,7 +69,7 @@ public class KeyValueServiceImplTest {
     @Test
     public void testCreateFileFromResource() throws IOException {
         try (InputStream resourceStream = getClass().getClassLoader().getResourceAsStream("default_data.txt")) {
-            
+
             // Ensure that the resource stream is not null (the resource file should exist in src/test/resources)
             assertNotNull(resourceStream, "Default resource file not found in the module's resources.");
 
@@ -73,17 +82,14 @@ public class KeyValueServiceImplTest {
 
             kvServiceImpl.createFileFromResource();
 
-            // Verify that fileHandler.saveToFile was called to save the loaded data
             verify(fileHandler, times(1)).saveToFile(eq("test.txt"), eq(mockData));
-            kvServiceImpl.print2DStructure();
-            
+
             assertEquals(mockData, kvServiceImpl.getData(), "Data loaded from resource should be set correctly.");
 
         } catch (IOException e) {
             fail("Error occurred while reading the resource file: " + e.getMessage());
         }
     }
-
 
     @Test
     public void testSearchPatt_Found() {
@@ -95,9 +101,12 @@ public class KeyValueServiceImplTest {
         kvServiceImpl.getData().addAll(data);
         SearchResult result = kvServiceImpl.searchPatt("p");
 
-        assertEquals(5, result.getCount());
-        assertEquals(4, result.getPositions().size());
-        assertArrayEquals(new String[]{"0x0", "0x1", "1x0", "1x1"}, result.getPositions().toArray());
+        assertAll("Search Pattern Test",
+                () -> assertEquals(5, result.getCount()),
+                () -> assertEquals(4, result.getPositions().size()),
+                () -> assertArrayEquals(new String[]{"0x0", "0x1", "1x0", "1x1"}, result.getPositions().toArray())
+        );
+
     }
 
     @Test
@@ -133,30 +142,41 @@ public class KeyValueServiceImplTest {
         kvServiceImpl.getData().add(row);
         kvServiceImpl.editKeyOrValue(0, 0, false, null, new String[]{"newKey", "newValue"});
         assertEquals(new KeyValuePair("newKey", "newValue"), kvServiceImpl.getData()
-            .get(0)
-            .getCells()
-            .get(0));
+                .get(0)
+                .getCells()
+                .get(0));
     }
 
     @Test
     public void testAddRow() {
-        kvServiceImpl.addRow(0, 2);
-        assertEquals(1, kvServiceImpl.getData().size());
-        assertEquals(2, kvServiceImpl.getData().get(0).getCells().size());
-    }
+        List<Row> data = List.of(
+                new Row(List.of(new KeyValuePair("key1", "vapor"), new KeyValuePair("pey2", "value2"))),
+                new Row(List.of(new KeyValuePair("pey1", "value"), new KeyValuePair("key2", "pops")))
+        );
 
-    @Test
-    public void testSearchPatt_EmptyString() {
-        kvServiceImpl.getData().add(new Row(List.of(new KeyValuePair("test", "value"))));
-        SearchResult result = kvServiceImpl.searchPatt("");
-        assertEquals(0, result.getCount());
+        kvServiceImpl.getData().addAll(data);
+
+        kvServiceImpl.addRow(2, 3);
+        System.out.println("Added row test");
+        kvServiceImpl.print2DStructure();
+
+        assertEquals(3, kvServiceImpl.getData().size());
+        assertEquals(3, kvServiceImpl.getData().get(2).getCells().size());
     }
 
     @Test
     public void testEditOutOfBounds() {
-        assertThrows(IndexOutOfBoundsException.class, () -> {
+        List<Row> data = List.of(
+                new Row(List.of(new KeyValuePair("key1", "vapor"), new KeyValuePair("pey2", "value2"))),
+                new Row(List.of(new KeyValuePair("pey1", "value"), new KeyValuePair("key2", "pops")))
+        );
+
+        kvServiceImpl.getData().addAll(data);
+
+        Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
             kvServiceImpl.editKeyOrValue(99, 99, true, "newKey", null);
         });
+        System.out.println("Exception message: " + exception.getMessage());
     }
 
     @Test
@@ -165,7 +185,6 @@ public class KeyValueServiceImplTest {
         kvServiceImpl.sortRow(0, "asc");
         assertEquals(0, kvServiceImpl.getData().get(0).getCells().size());
     }
-
 
     @Test
     public void testSortRow() {
@@ -180,17 +199,17 @@ public class KeyValueServiceImplTest {
 
         kvServiceImpl.sortRow(0, "asc");
 
-        assertArrayEquals(new String[]{"aab", "aBa", "bAb"}, 
-            kvServiceImpl.getData().get(0).getCells()
-            .stream()
-            .map(KeyValuePair::getKey)
-            .toArray());
+        assertArrayEquals(new String[]{"aab", "aBa", "bAb"},
+                kvServiceImpl.getData().get(0).getCells()
+                        .stream()
+                        .map(KeyValuePair::getKey)
+                        .toArray());
 
         kvServiceImpl.sortRow(1, "desc");
         assertArrayEquals(new String[]{"W!2", "Q#O", "]j9"}, kvServiceImpl.getData().get(1).getCells()
-            .stream()
-            .map(KeyValuePair::getKey)
-            .toArray());
+                .stream()
+                .map(KeyValuePair::getKey)
+                .toArray());
 
         System.out.println("After 2D Table Structure");
         kvServiceImpl.print2DStructure();
